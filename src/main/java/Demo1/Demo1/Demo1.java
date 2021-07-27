40abb9815eadcc0a16bac803b531becb1c473700
prajaktaka <prajaktaka@prajaktaka-w10.cybage.com>
2021-07-27 11:03:34 +0530

    commit1

--- src/main/java/Demo1/Demo1/Demo1.java
+++ src/main/java/Demo1/Demo1/Demo1.java
@@ -1,8 +1,9 @@
 package Demo1.Demo1;
 
-import java.util.concurrent.TimeUnit; 
-import org.junit.AfterClass; 
-import org.junit.BeforeClass; 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test; 
 import org.openqa.selenium.By; 
 import org.openqa.selenium.WebDriver; 
@@ -13,14 +14,16 @@ public class Demo1
 {   
 	static WebDriver driver; 
 	
-	@BeforeClass public static void BrowserOpen() 
+	@Before
+	public  void BrowserOpen() 
 	{
-		System.setProperty("webdriver.chrome.driver", "D:/Selenium/chromedriver.exe");     
+		System.setProperty("webdriver.chrome.driver", "D:/Selenium/Chrome/chromedriver.exe");     
 		driver= new ChromeDriver() ;     
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); 
 	}
 	
-	@Test public void Register_User() 
+	@Test
+	public void Register_User() 
 	{
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); 
 		driver.get("https://opensource-demo.orangehrmlive.com/");
@@ -32,8 +35,9 @@ public class Demo1
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	} 
 
-	@AfterClass public static void BrowserClose() 
+	@After
+	public  void BrowserClose() 
 	{
-		driver.quit(); 
+		//driver.quit(); 
 	}   
 }
